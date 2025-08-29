4cf39a4a46543abd4d0dee001a5f71a5bf870b29
wibetter <liudan@neocrm.com>
2025-08-29 16:18:15 +0800

    fix: 调整组件示例中的plugin数据

--- demo/vue-info-card/plugin.js
+++ demo/vue-info-card/plugin.js
@@ -1,23 +1,35 @@
 /**
  * @file 自定义组件对接编辑器的描述文件
  */
+// @ts-ignore
 import { registerNeoEditorPlugin } from '../../dist/index';
 
+// 自定义组件名称，用于标识组件的唯一性
+const WidgetCmpType = 'vue-info-card';
 export class InfoCardPlugin {
-  // 关联渲染器名字
-  cmpType = 'vue-info-card';
-  name = '信息卡片';
-  description = '信息卡片';
+  cmpType = WidgetCmpType;
+  name = 'vue 信息卡片';
+  description = '信息展示卡片';
   tags = ['自定义'];
   icon = 'fa fa-file-code-o';
-
+  
+  // 初次插入页面的默认数据
   scaffold = {
-    type: 'vue-info-card',
-    label: '信息卡片',
+    cmpType: WidgetCmpType,
+    props: {
+      title: '营销服全场景智能CRM，帮助企业搭建数字化客户经营平台，实现业绩高质量增长。',
+      label: '信息卡片',
+      backgroundImage: 'https://neo-widgets.bj.bcebos.com/NeoCRM.jpg',
+      img_count: 3,
+      comment_count: 2021
+    },
   };
+  // 设计器端预览时展示的默认数据
   previewSchema = {
-    type: 'vue-info-card',
-    label: '信息卡片',
+    cmpType: WidgetCmpType,
+    props: {
+      label: 'vue 信息卡片'
+    }
   };
 
   panelTitle = '配置';
@@ -34,8 +46,7 @@ export class InfoCardPlugin {
       type: 'text',
       name: 'backgroundImage',
       label: '展示图片',
-      value:
-        'https://neo-widgets.bj.bcebos.com/NeoCRM.jpg',
+      value: 'https://neo-widgets.bj.bcebos.com/NeoCRM.jpg',
     },
     {
       type: 'input-number',
@@ -47,7 +58,7 @@ export class InfoCardPlugin {
       type: 'input-number',
       name: 'comment_count',
       label: '评论数',
-      value: 2025,
+      value: 2021,
     },
   ];
 }
@@ -55,3 +66,4 @@ export class InfoCardPlugin {
 registerNeoEditorPlugin(InfoCardPlugin);
 
 export default InfoCardPlugin;
+
--- demo2/react-info-card/plugin.js
+++ demo2/react-info-card/plugin.js
@@ -3,56 +3,63 @@
  */
 import { registerNeoEditorPlugin } from '../../src/main';
 
+// 自定义组件名称，用于标识组件的唯一性
+const WidgetCmpType = 'react-info-card';
 export class InfoCardPlugin {
-  // 关联渲染器名字
-  cmpType = 'react-info-card';
-  name = 'react组件';
-  description = '信息卡片';
+  cmpType = WidgetCmpType;
+  name = 'react 信息卡片';
+  description = '信息展示卡片';
   tags = ['自定义'];
-
   icon = 'fa fa-file-code-o';
-  order = 99;
+  
+  // 初次插入页面的默认数据
   scaffold = {
-    type: 'react-info-card',
-    label: '信息卡片'
+    cmpType: WidgetCmpType,
+    props: {
+      title: '营销服全场景智能CRM，帮助企业搭建数字化客户经营平台，实现业绩高质量增长。',
+      label: '信息卡片',
+      backgroundImage: 'https://neo-widgets.bj.bcebos.com/NeoCRM.jpg',
+      img_count: 3,
+      comment_count: 2021
+    },
   };
+  // 设计器端预览时展示的默认数据
   previewSchema = {
-    type: 'react-info-card',
-    label: '信息卡片'
+    cmpType: WidgetCmpType,
+    props: {
+      label: 'react 信息卡片'
+    }
   };
 
   panelTitle = '配置';
 
-  panelBodyCreator = (context) => {
-    return [
-      {
-        type: 'textarea',
-        name: 'title',
-        label: '卡片title',
-        value:
-          '营销服全场景智能CRM，帮助企业搭建数字化客户经营平台，实现业绩高质量增长。',
-      },
-      {
-        type: 'text',
-        name: 'backgroundImage',
-        label: '展示图片',
-        value:
-          'https://neo-widgets.bj.bcebos.com/NeoCRM.jpg',
-      },
-      {
-        type: 'input-number',
-        name: 'img_count',
-        label: '图片数量',
-        value: 3,
-      },
-      {
-        type: 'input-number',
-        name: 'comment_count',
-        label: '评论数',
-        value: 2025,
-      },
-    ];
-  };
+  panelControls = [
+    {
+      type: 'textarea',
+      name: 'title',
+      label: '卡片title',
+      value:
+        '营销服全场景智能CRM，帮助企业搭建数字化客户经营平台，实现业绩高质量增长。',
+    },
+    {
+      type: 'text',
+      name: 'backgroundImage',
+      label: '展示图片',
+      value: 'https://neo-widgets.bj.bcebos.com/NeoCRM.jpg',
+    },
+    {
+      type: 'input-number',
+      name: 'img_count',
+      label: '图片数量',
+      value: 3,
+    },
+    {
+      type: 'input-number',
+      name: 'comment_count',
+      label: '评论数',
+      value: 2021,
+    },
+  ];
 }
 
 registerNeoEditorPlugin(InfoCardPlugin);
